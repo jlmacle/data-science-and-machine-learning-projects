b82567cd783d124a1c0ebbd65a73b176ae20c55f
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-06-09 13:30:33 -0500

    Code

--- MachineLearningRepository_Adult-dataset/1.DataCleaningAndProcessing.py
+++ MachineLearningRepository_Adult-dataset/1.DataCleaningAndProcessing.py
@@ -1,6 +1,7 @@
 from ds_ml_utils.data_cleaning_and_processing import DataCleaningAndProcessing as dcp_class
 from ds_ml_utils.data_visualization import DataVisualization as dv_class
 from _DataPath import _DataPath as _data_path_class
+import os
 
 
 # Cleaning the data before processing
@@ -9,13 +10,19 @@ dv = dv_class()
 _data_path = _data_path_class()
 data_path = _data_path.get_data_path_object()
 
-# Removing rows with duplicates and unvavailable data 
-dcp.drop_duplicates_and_remove_rows_with_data_unavailable_from_csv_file(data_path.get_path_to_csv_folder(), data_path.get_file_name_for_csv_with_original_data(),low_memory_setting=False)
+# # Removing rows with duplicates and unvavailable data 
+df  = dcp.drop_duplicates_and_remove_rows_with_data_unavailable_from_csv_file(data_path.get_path_to_csv_folder(), data_path.get_file_name_for_csv_with_original_data(),low_memory_setting=False)
 
-df= dcp.import_csv_to_df( data_path.get_path_to_cleaned_csv_file(),low_memory_setting=False)
-# Pre-cleaning before using a pivot table
+# # Replacing "marital status" with "marital-status" in the csv file
+# # to avoid an issue at "table_data_to_csv" time
+df = df.replace("Marital status", "Marital-status")
+df = df.replace("Capital gain", "Capital-gain")
+df = df.replace("Capitl loss", "Capital-loss")
+
+# # Pre-cleaning before using a pivot table
 # File used : adult_na_and_duplicates_removed.csv
 results = dcp.pre_pivot_table_pre_cleaning(df, "Education-Num", "Education",print_data_for_unique_values=False)
+# df is untouched by this step
 print()
 print("Data in need of cleaning")
 print(results)
@@ -24,9 +31,13 @@ if(results != {}):
     print()
     lines = dcp.line_finding_given_labels_and_column(df,"Education", [" Bachelors2", " Masters2"," Some-college2"])
     print()
-# Cleaning the data and re-setting the value for the cleaned data file name
-# New file name used in data_path.get_path_to_cleaned_csv_file(): 
-# adult_na_and_duplicates_removed-education_column_cleaned.csv
+
+df.to_csv(os.path.join(data_path.get_path_to_csv_folder(),"adult-cleaned_data_if_previous_result_was_an_empty_dict.csv"))
+
+# Checking the program with the cleaned data
+
+# Re-setting the value for the cleaned data file name
+
 
     
 
