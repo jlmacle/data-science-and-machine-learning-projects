3332cacdf0ae45e4fbf716a42f5071f04bf214cd
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-06-12 14:30:48 -0500

    Code cleanup

--- OFLC_H1B-dataset/_DataPath.py
+++ OFLC_H1B-dataset/_DataPath.py
@@ -9,8 +9,7 @@ class _DataPath:
         self.data_path.set_file_name_for_csv_with_original_data("Data_Restricted_to_computer_related_fields.csv")
         # self.data_path.set_file_name_for_csv_with_cleaned_data("Data_Restricted_to_computer_related_fields-rows_with_commas_only_removed.csv")
         self.data_path.set_file_name_for_csv_with_cleaned_data("Data_Restricted_to_computer_related_fields-rows_with_commas_only_removed-Job_titles_concatenated.csv")
-        self.data_path.set_path_to_cleaned_csv_file(os.path.join(self.data_path.get_path_to_csv_folder(), self.data_path.get_file_name_for_csv_with_cleaned_data()))
-            
+                    
         self.data_path.set_path_to_folder_with_data_for_reporting(os.path.join(os.getcwd(),"OFLC_H1B-dataset","data_for_report"))       
         self.data_path.set_file_name_for_csv_file_with_data_for_reporting("data_for_report.csv")
         self.data_path.set_file_name_for_txt_file_with_data_for_reporting("data_for_report.txt")
