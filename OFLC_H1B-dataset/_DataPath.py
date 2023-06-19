55429730dd0890752b178add7fbad449f2771144
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-06-19 13:53:25 -0500

    Removed columns from the dataset for readibility of reporting data

--- OFLC_H1B-dataset/_DataPath.py
+++ OFLC_H1B-dataset/_DataPath.py
@@ -6,7 +6,7 @@ class _DataPath:
         self.data_path = data_path_class()
 
         self.data_path.set_path_to_data_folder(os.path.join(os.getcwd(),"OFLC_H1B-dataset","data"))
-        self.data_path.set_file_name_for_csv_with_original_data("Data_Restricted_to_computer_related_fields.csv")
+        self.data_path.set_file_name_for_csv_with_original_data("1.Data_Restricted_to_computer_related_positions-Some_columns_removed.csv")
         self.data_path.set_file_name_for_csv_with_cleaned_data("cleaned_data.csv")
                     
         self.data_path.set_path_to_folder_with_data_for_reporting(os.path.join(os.getcwd(),"OFLC_H1B-dataset","data_for_report"))       
